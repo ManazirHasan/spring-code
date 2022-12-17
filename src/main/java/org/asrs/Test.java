5b3f7f60d2baf7be1a72cbb3a9cbebef5cd9ccce
Manazir <mkhanj2ee@gmail.com>
2022-12-17 21:00:19 +0530

    testing

---
+++ src/main/java/org/asrs/Test.java
@@ -0,0 +1,17 @@
+package org.asrs;
+
+public class Test {
+
+    public static void main(String[] args) {
+        int[] x = {2,-4,6,-7,9,1};//-4,2,6,9,1,-7
+        //
+        int i=0;
+        int j=2;
+        while(i<=j){
+        }
+
+
+
+
+    }
+}
